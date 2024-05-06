6e1c0ac390aeea3df6fb66b6e956ad9439673d0a
aramdahmeni <148446627+aramdahmeni@users.noreply.github.com>
2024-05-06 11:26:06 +0100

    controlers w cruds

--- src/Controller/AdministrateurController.php
+++ src/Controller/AdministrateurController.php
@@ -2,17 +2,80 @@
 
 namespace App\Controller;
 
+use App\Entity\Administrateur;
+use App\Form\AdministrateurType;
+use App\Repository\AdministrateurRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/administrateur')]
 class AdministrateurController extends AbstractController
 {
-    #[Route('/administrateur', name: 'app_administrateur')]
-    public function index(): Response
+    #[Route('/', name: 'app_administrateur_index', methods: ['GET'])]
+    public function index(AdministrateurRepository $administrateurRepository): Response
     {
         return $this->render('administrateur/index.html.twig', [
-            'controller_name' => 'AdministrateurController',
+            'administrateurs' => $administrateurRepository->findAll(),
         ]);
+    }
+
+    #[Route('/new', name: 'app_administrateur_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $administrateur = new Administrateur();
+        $form = $this->createForm(AdministrateurType::class, $administrateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($administrateur);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_administrateur_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('administrateur/new.html.twig', [
+            'administrateur' => $administrateur,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_administrateur_show', methods: ['GET'])]
+    public function show(Administrateur $administrateur): Response
+    {
+        return $this->render('administrateur/show.html.twig', [
+            'administrateur' => $administrateur,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_administrateur_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Administrateur $administrateur, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(AdministrateurType::class, $administrateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_administrateur_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('administrateur/edit.html.twig', [
+            'administrateur' => $administrateur,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_administrateur_delete', methods: ['POST'])]
+    public function delete(Request $request, Administrateur $administrateur, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$administrateur->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($administrateur);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_administrateur_index', [], Response::HTTP_SEE_OTHER);
     }
 }
---
+++ src/Form/EnseignantType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Enseignant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EnseignantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('typeENS')
+            ->add('codeENS')
+            ->add('nbAnneeExp')
+            ->add('matiere')
+            ->add('idClasses')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Enseignant::class,
+        ]);
+    }
+}
